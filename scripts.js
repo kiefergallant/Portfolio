95d7c953f4011d3036640a6a20ee7885fd92fecb
kiefergallant <44451951+kiefergallant@users.noreply.github.com>
2021-01-30 01:31:04 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// These functions open and close the mobile navigation
+function openMobileNav(){
+  document.getElementById('mobilenav').style.width='100%';
+}
+
+function closeMobileNav(){
+  document.getElementById('mobilenav').style.width='0';
+}
